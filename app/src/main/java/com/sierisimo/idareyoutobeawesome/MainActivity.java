590c5ea1e2ed07e2fad0804115890193412dbda1
Sinuhe Jaime <sierisimo@gmail.com>
2017-01-21 13:47:33 -0600

    starts #3 -> Need of main activity

---
+++ app/src/main/java/com/sierisimo/idareyoutobeawesome/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sierisimo.idareyoutobeawesome;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sierisimo on 1/21/17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}

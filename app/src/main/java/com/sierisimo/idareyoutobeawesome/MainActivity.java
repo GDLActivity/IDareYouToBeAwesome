26d084a0465e71eed58e552e7633ebf2fd7398cf
Sinuhe Jaime <sierisimo@gmail.com>
2017-01-25 16:50:28 -0600

    Added a toolbar and also refactor to some code and names this commit also fixes #11 #22

--- app/src/main/java/com/sierisimo/idareyoutobeawesome/MainActivity.java
+++ app/src/main/java/com/sierisimo/idareyoutobeawesome/MainActivity.java
@@ -2,7 +2,9 @@ package com.sierisimo.idareyoutobeawesome;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -20,12 +22,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initActivity();
+    }
+
+    private void initActivity() {
+        setupToolbar();
+
         String[] mTitles = new String[]{"Title One", "Title Two", "Title Three"};
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         ListView mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
         mDrawerList.setAdapter(new ArrayAdapter<>(this, R.layout.item_drawer_list, mTitles));
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener(getApplicationContext()));
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true);
+            actionbar.setHomeAsUpIndicator(R.drawable.vector_menubar_hamburger);
+            actionbar.setDisplayShowTitleEnabled(true);
+            actionbar.setTitle(R.string.title_dummy);
+        }
     }
 
     @Override

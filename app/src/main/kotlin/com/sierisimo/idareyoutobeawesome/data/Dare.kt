dcf0a8732d25a04d6d2fef80611ed7136183c5cf
Sinuhe Jaime <sierisimo@gmail.com>
2017-01-26 12:01:07 -0600

    Added a small JavaDoc and also fix #17

--- app/src/main/kotlin/com/sierisimo/idareyoutobeawesome/data/Dare.kt
+++ app/src/main/kotlin/com/sierisimo/idareyoutobeawesome/data/Dare.kt
@@ -1,6 +1,12 @@
 package com.sierisimo.idareyoutobeawesome.data
 
 /**
+ *
  * Created by sierisimo on 1/26/17.
+ */
+
+/**
+ * This class represent a current Dare fetch by the server
+ * This will be used everywhere, it's the main type of object used by viewholders and detail views.
  */
 data class Dare(val id: Long, val title: String, val description: String, val dateCreated: Long, val dateSolved: Long)

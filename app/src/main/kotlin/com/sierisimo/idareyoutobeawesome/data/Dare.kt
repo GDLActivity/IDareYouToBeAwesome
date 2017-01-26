2bff1b2afe6dc83207219f6a9e59808eeba69b15
Sinuhe Jaime <sierisimo@gmail.com>
2017-01-26 11:52:04 -0600

    Added Dare class

---
+++ app/src/main/kotlin/com/sierisimo/idareyoutobeawesome/data/Dare.kt
@@ -0,0 +1,6 @@
+package com.sierisimo.idareyoutobeawesome.data
+
+/**
+ * Created by sierisimo on 1/26/17.
+ */
+data class Dare(val id: Long, val title: String, val description: String, val dateCreated: Long, val dateSolved: Long)

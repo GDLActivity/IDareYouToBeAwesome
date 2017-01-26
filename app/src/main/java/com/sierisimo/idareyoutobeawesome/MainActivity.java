553fbd914e083858a19ba9b3ecd34324f1c0cfff
balthair94 <archerbaltazar@gmail.com>
2017-01-26 00:41:16 -0600

    fix #21 NavigationDrawer added to the DrawerLayout

--- app/src/main/java/com/sierisimo/idareyoutobeawesome/MainActivity.java
+++ app/src/main/java/com/sierisimo/idareyoutobeawesome/MainActivity.java
@@ -1,12 +1,16 @@
 package com.sierisimo.idareyoutobeawesome;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.sierisimo.idareyoutobeawesome.adapters.RecyclerViewChallengeAdapter;
 import com.sierisimo.idareyoutobeawesome.listeners.DrawerItemClickListener;
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private String[] mTitles;
     private String[] mChallenges;
     private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.LayoutManager mLayoutManager;
@@ -32,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
         mTitles = new String[]{"Title One", "Title Two", "Title Three"};
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationViewClick();
 
         mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
         mChallenges = new String[]{"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"};
@@ -41,6 +48,36 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new RecyclerViewChallengeAdapter(mChallenges);
         mRecyclerView.setAdapter(mAdapter);
 
+    }
+
+    public void navigationViewClick(){
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+
+                switch (item.getItemId()){
+                    case R.id.title_one:
+                        Toast.makeText(getApplicationContext(), "Title One", Toast.LENGTH_SHORT).show();
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    case R.id.title_two:
+                        Toast.makeText(getApplicationContext(), "Title Two", Toast.LENGTH_SHORT).show();
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    case R.id.title_three:
+                        Toast.makeText(getApplicationContext(), "Title Three", Toast.LENGTH_SHORT).show();
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 
     @Override

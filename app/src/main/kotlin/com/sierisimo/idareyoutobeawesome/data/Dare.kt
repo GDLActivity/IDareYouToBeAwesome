883a8fc9cc22699ea27da32344f68c59114f3169
Sinuhe Jaime <sierisimo@gmail.com>
2017-01-26 12:12:47 -0600

    Started the dare-provider

--- app/src/main/kotlin/com/sierisimo/idareyoutobeawesome/data/Dare.kt
+++ app/src/main/kotlin/com/sierisimo/idareyoutobeawesome/data/Dare.kt
@@ -1,5 +1,7 @@
 package com.sierisimo.idareyoutobeawesome.data
 
+import com.sierisimo.idareyoutobeawesome.util.USE_MOCKS
+
 /**
  *
  * Created by sierisimo on 1/26/17.
@@ -10,3 +12,14 @@ package com.sierisimo.idareyoutobeawesome.data
  * This will be used everywhere, it's the main type of object used by viewholders and detail views.
  */
 data class Dare(val id: Long, val title: String, val description: String, val dateCreated: Long, val dateSolved: Long)
+
+object DareProvider {
+    fun getDares(): List<Dare> {
+        return if (USE_MOCKS) getMockDares()
+        else fetchDares()
+    }
+
+    private fun getMockDares(): List<Dare> = emptyList()
+
+    private fun fetchDares(): List<Dare> = emptyList()
+}
---
+++ app/src/main/kotlin/com/sierisimo/idareyoutobeawesome/util/Globals.kt
@@ -0,0 +1,8 @@
+package com.sierisimo.idareyoutobeawesome.util
+
+/**
+ * Created by sierisimo on 1/26/17.
+ *
+ */
+
+const val USE_MOCKS = true
